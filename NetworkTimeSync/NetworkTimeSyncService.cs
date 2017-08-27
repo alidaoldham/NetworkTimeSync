d5bb238c85a5d11e54500e7b0b624b948e04a5e6
AlidaOldham <alida.oldham@gmail.com>
2017-08-27 10:00:10 -0600

    NTS-1 - Wired up the Network Time Sync service to start and stop the update time runner as requested by the system

--- NetworkTimeSync/NetworkTimeSyncService.cs
+++ NetworkTimeSync/NetworkTimeSyncService.cs
@@ -1,20 +1,34 @@
 ﻿using System.ServiceProcess;
+using Common.Logging;
+using NetworkTimeSync.TimeServices.NetworkTimeService;
+using NetworkTimeSync.TimeServices.WindowsTimeService;
+using NetworkTimeSync.UpdateTime;
 
 namespace NetworkTimeSync
 {
 	public partial class NetworkTimeSyncService : ServiceBase
 	{
+	    private static readonly ILog Log = LogManager.GetLogger(typeof(NetworkTimeSyncService));
+	    private readonly UpdateTimeRunner runner;
+
 		public NetworkTimeSyncService()
 		{
 			InitializeComponent();
+            var networkTimeService = new TimeZoneDbService("YourApiKeyHere");
+            var windowsTimeService = new WindowsTimeServiceImpl(new Win32ExternalImpl());
+            runner = new UpdateTimeRunner(networkTimeService, windowsTimeService);
 		}
 
 		protected override void OnStart(string[] args)
 		{
+            Log.Trace("Starting Network Time Sync service");
+            runner.Start();
 		}
 
 		protected override void OnStop()
 		{
-		}
+		    Log.Trace("Stopping Network Time Sync service");
+            runner.Stop();
+        }
 	}
 }
